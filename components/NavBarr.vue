798090966e37bead672cdd9762f9ca79b1a654ca
Alberto Bettini <alberto.bettini@marconirovereto.it>
2023-05-10 20:37:47 +0200

    first commit

---
+++ components/NavBarr.vue
@@ -0,0 +1,221 @@
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+});
+</script>
+
+<template>
+  <div>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+      <div
+        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+      >
+        <a href="#" class="flex items-center">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            class="h-8 mr-3"
+            alt="Flowbite Logo"
+          />
+          <span
+            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+            >Flowbite</span
+          >
+        </a>
+        <button
+          data-collapse-toggle="navbar-multi-level"
+          type="button"
+          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-multi-level"
+          aria-expanded="false"
+        >
+          <span class="sr-only">Open main menu</span>
+          <svg
+            class="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-multi-level">
+          <ul
+            class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          >
+            <li v-for="(item, index) in items" :key="index">
+              <nuxt-link
+                v-if="!item.child"
+                :to="{ path: item.path, hash: item.hash }"
+                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                aria-current="page"
+              >
+                {{ item.name }}
+              </nuxt-link>
+              <div v-else>
+                <button
+                  id="dropdownNavbarLink"
+                  data-dropdown-toggle="dropdownNavbar"
+                  class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                >
+                  {{ item.name }}
+                  <svg
+                    class="w-5 h-5 ml-1"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+                <!-- DropDownChild menu -->
+                <div
+                  id="dropdownNavbar"
+                  class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                >
+                  <ul
+                    class="py-2 text-sm text-gray-700 dark:text-gray-400"
+                    aria-labelledby="dropdownLargeButton"
+                  >
+                    <div v-for="(y, index) in item.child" :key="index">
+                      <li v-if="!y.child">
+                        <nuxt-link
+                          :to="{ path: y.path, hash: y.hash }"
+                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          {{ y.name }}
+                        </nuxt-link>
+                      </li>
+                      <li v-else
+                      aria-labelledby="dropdownNavbarLink">
+                        <button
+                          id="doubleDropdownButton"
+                          data-dropdown-toggle="doubleDropdown"
+                          data-dropdown-placement="right-start"
+                          type="button"
+                          class="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Dropdown<svg
+                            aria-hidden="true"
+                            class="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                              clip-rule="evenodd"
+                            ></path>
+                          </svg>
+                        </button>
+                        <div
+                          id="doubleDropdown"
+                          class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                        >
+                          <ul
+                            class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                            aria-labelledby="doubleDropdownButton"
+                          >
+                            <li v-for="(z, index) in y.child" :key="index">
+                              <nuxt-link
+                              :to="{ path: z.path, hash: z.hash }"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+                              >
+                                {{(z.name)}}
+                              </nuxt-link>
+                            </li>
+                          </ul>
+                        </div>
+                      </li>
+                    </div>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+      items: [
+        {
+          name: "HOME",
+          path: "/",
+          hash: "#home",
+          child: [
+            {
+              name: "link1",
+              path: "/",
+              hash: "#link1",
+            },
+            {
+              name: "link2",
+              path: "/",
+              hash: "#link2",
+            },
+            {
+              name: "aboutUs",
+              path: "/",
+              hash: "#home",
+            },
+            {
+              name: "home",
+              path: "/",
+              hash: "#home",
+              child:[
+              {
+                name: "llink 3",
+                path: "/",
+                hash: "#home",
+              },
+              {
+                name: "llink 4",
+                path: "/",
+                hash: "#home",
+              },
+              ],
+            },
+          ],
+        },
+        {
+          name: "aboutUs",
+          path: "/",
+          hash: "#home",
+        },
+        {
+          name: "LINK",
+          path: "/",
+          hash: "#link",
+        },
+        {
+          name: "ciao",
+          path: "/",
+          hash: "#link",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import NavBarr from '~/components/NavBarr.vue';
+
+</script>
+
+<template>
+  <div>
+    <NavBarr></NavBarr>
+  </div>
+</template>
+
+<style scoped></style>
